bc610b14c9f21012ec58a13b4e40b13e69661935
DiveshRW <divesh20july@gmail.com>
2023-06-27 23:45:22 +0530

    Implement auth and table migration factory,seeder for role and users

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,15 @@
 
 namespace App\Models;
 
-use Backpack\CRUD\app\Models\Traits\CrudTrait;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class User extends Authenticatable
 {
-    use CrudTrait;
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
@@ -23,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+         'role_id',
     ];
 
     /**
@@ -44,4 +44,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+        public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
